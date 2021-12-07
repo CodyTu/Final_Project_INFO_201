fa83319e715bf1d78aaf2be1da978b53c122a096
Andrew Chen <andrewdc@uw.edu>
2021-12-06 18:25:42 -0800

    added introduction section

--- app.r
+++ app.r
@@ -2,7 +2,23 @@ library(shiny)
 
 intro <- tabPanel(
   "Introduction",
-  titlePanel("Introduction")
+  img("", src="https://th.bing.com/th/id/OIP.oEEcPnzQJgzlxI1DpoTduAHaBa?pid=ImgDet&rs=1"),
+  h2("Introduction"),
+  p("In this project, we are interested in investigating the relationship between education and socioeconomic factors in the United States. We 
+    are interested in this topic because there are many disparities in education quality and outcomes between different communities throughout 
+    the US. We think that it may be valuable to use data to draw conclusions about the factors that that could cause or contribute
+    to these disparities."),
+  
+  h3("Questions"),
+  p("What socioeconomic factors most strongly influence educational attainment?"),
+  p("How much do certain socioeconomic factors affect education?"),
+  p("What aspects of educational achievement do socioeconomic factors affect?"),
+  
+  h3("Data"),
+  p("The dataset that we are using comes from ", a("Kaggle", href="https://www.kaggle.com/noriuk/us-education-datasets-unification-project"), 
+  " and was posted by user Roy Garrard. It includes data covering state revenue, academic achievement, and enrollment, which is also broken down 
+  further into race and gender. Titled U.S. Education Datasets: Unification Project, the data is aggregated from multiple .gov sites, such as 
+  The Nation's Report Card, The US Census Bureau, and the National Center for Education Statistics.")
 )
 
 page_one <- tabPanel(
